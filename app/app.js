47e0ec08fe72f3f561d2a4f8709f1c71a24eff3f
ThisIsRahmat <thisisrahmat@gmail.com>
2022-12-11 05:37:47 +0000

    haven't gotten it working but have a basic frontend, adding some styling in case I want to revert back to this same style'

--- app/app.js
+++ app/app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const http = require("http");
@@ -9,27 +8,28 @@ const port = 5000;
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-
+// Use body-parser to parse form data
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // index page
 
 app.get('/', function (req, res) {
-  res.render('/pages/index'); 
-})
+  res.render('pages/index'); 
+});
 
 // about page
 app.get('/about', function(req, res) {
   res.render('pages/about');
 });
 
-// about page
-app.get('/generate-test', function(req, res) {
-  res.render('pages/test');
+// Set up route for test generation form
+app.get('/generate-test', (req, res) => {
+  // Render form for generating test questions
+  res.render('pages/generate-test-questions');
 });
 
-// testing page
-// Set up route for test generation
-app.get('/generate-test', (req, res) => {
+// Handle form submission for generating test questions
+app.post('/generate-test', (req, res) => {
   // Get user input from form
   const style = req.body.style;
   const subject = req.body.subject;
@@ -48,41 +48,12 @@ app.get('/generate-test', (req, res) => {
     function(error, response) {
       if (error) throw error;
       // Use generated test questions to create test
-      const test = createQuestion(response.choices[0].text);
-      // Send test back to client
-      res.send(test);
+      const test = createTest(response.choices[0].text);
+      // Render test in quizlet format
+      res.render('pages/quiz', { test: test });
     }
   );
 });
-
-// app.post('/generate-test', (req, res) => {
-// }
-
-
-//Function to generate questions 
-
-function createQuestions(subject, topic, qualification, examBoard){
-    // sessionToken is required; see below for details
-    const api = new ChatGPTAPI({
-      sessionToken: process.env.SESSION_TOKEN
-    })
-
-
-  //     // ensure the API is properly authenticated
-  //  await api.ensureAuth()
-
-  const conversation = api.sendMessage()
-
-
-  // send a message and wait for the response
-  const questions = await api.sendMessage(
-
-    'Generate a list of 10' + style + 'questions on the topic of ' + topic + 'for a' +  qualification + ' exam from the ' + examBoard + '
-    );
-  
-  console.log(questions)
-  return questions
-}
 
 // Function to create test from generated questions
 function createTest(questions) {
@@ -105,4 +76,4 @@ app.use((err, req, res, next) => {
 
 app.listen(port, () => {
   console.log('Test generator listening on port ' + port + '!');
-});
+}); 
